8ca171feb4bfab671ee01ab63bc955f910efcc06
Taurintas Kudirka <taurisx@gmail.com>
2018-10-03 16:11:32 +0300

    Added placeholder list items before they are loaded from api

--- src/appView.js
+++ src/appView.js
@@ -2,6 +2,52 @@ function AppView() {
     var me = this,
         list = document.getElementById('list'),
 
+    emptyListItem = function() {
+        var placeholderColor = '#b3b3cc';
+        var listItem = document.createElement('div');
+        listItem.classList.add('column', 'is-full-mobile', 'is-half-tablet', 'is-one-third-widescreen', 'is-one-quarter-fullhd');
+
+        var box = document.createElement('div');
+        box.classList.add('box');
+        box.style.backgroundColor = '#d1d1e0';
+        {
+            var position = document.createElement('div');
+            position.classList.add('website-rank');
+            position.style.width = '6rem';
+            position.style.height = '1.9rem';
+            position.style.marginTop = '0.65rem';
+            position.style.marginBottom = '0.65rem';
+            position.style.backgroundColor = placeholderColor
+            box.appendChild(position);
+
+            var icon = document.createElement('div');
+            icon.classList.add('website-icon', 'center');
+            icon.style.backgroundColor = placeholderColor
+            box.appendChild(icon);
+            
+            var name = document.createElement('div');
+            name.classList.add('website-name', 'center');
+            name.style.width = '80%';
+            name.style.height = '2rem';
+            name.style.marginTop = '0.6rem';
+            name.style.marginBottom = '0.6rem';
+            name.style.backgroundColor = placeholderColor
+            box.appendChild(name);
+
+            var count = document.createElement('div');
+            count.classList.add('website-count', 'center');
+            count.style.width = '40%';
+            count.style.height = '1.4rem';
+            count.style.marginTop = '0.2rem';
+            count.style.marginBottom = '0.4rem';
+            count.style.backgroundColor = placeholderColor
+            box.appendChild(count);
+        }
+        listItem.appendChild(box);
+
+        return listItem;
+    },
+
     websitePositionElement = function(itemData) {
         var position = document.createElement('div');
         position.classList.add('website-rank');
@@ -28,7 +74,7 @@ function AppView() {
         var item;
         list.innerHTML = '';
         for (var i = 0; i < 4; i++) {
-            item = me.listItem({text: (i+1).toString() + ' Box'});
+            item = emptyListItem();
             list.appendChild(item);
         }
     }
